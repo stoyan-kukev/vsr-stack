f861cc63b392d332bb29e1c894000bc5d325e9e6
stoyan-kukev <stoykukev@proton.me>
2024-06-23 14:51:05 +0300

    prettier fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { NavBar } from './components/NavBar';
-import { ThemeProvider } from './components/theme-provider';
+import { NavBar } from "./components/NavBar";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <NavBar />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<NavBar />
+		</ThemeProvider>
+	);
 }
 
 export default App;
