65232b192a501aad5c302500d8bb112399c9890b
s460 <66819532+S460s@users.noreply.github.com>
2024-06-23 14:45:54 +0300

    cooking

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { UserNav } from './components/UserNav'
+import { NavBar } from './components/NavBar';
+import { ThemeProvider } from './components/theme-provider';
 
 function App() {
   return (
-    <UserNav />
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <NavBar />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
